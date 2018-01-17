95f08c6883679d972686d30d413dcfd3ea08cb15
yaravireyna <34288584+yaravireyna@users.noreply.github.com>
2018-01-17 12:27:30 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
